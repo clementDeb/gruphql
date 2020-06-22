95346ee1118da951a4eca4f203514e193236736c
clement <clem.debrosse@gmail.com>
2020-06-22 22:47:06 +0200

    add next step comment

--- src/main/java/com/gowyn/controller/Controller.java
+++ src/main/java/com/gowyn/controller/Controller.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/v1/gowyn")
 public class Controller {
 
+    //use AOP to validate the request before going into the getMethod
+
     @GetMapping("/{id}")
     public String getById(@PathVariable Long id, @RequestBody String request) {
         return "gowyn request " + request;
